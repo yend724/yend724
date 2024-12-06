5c22d62d4ca78efe96507f7016e6975f6367da7c
yend724 <yend9524@gmail.com>
2024-12-06 21:36:08 +0900

    wip

--- src/features/my-works/constants/index.ts
+++ src/features/my-works/constants/index.ts
@@ -10,13 +10,13 @@ export const MY_WORKS: Work[] = [
     url: 'https://github.com/yend724/shake-snap',
   },
   {
-    title: 'Vite Plugin Ssinc',
-    description: 'ViteでSSIライクな `#include` をするためのプラグイン',
-    url: 'https://www.npmjs.com/package/vite-plugin-ssinc',
-  },
-  {
     title: 'GitHub Actions Cron Expression Builder',
     description: 'GitHub ActionsのCron式を簡単に作成するツール',
     url: 'https://cron-expression-builder.yend.dev',
+  },
+  {
+    title: 'Vite Plugin Ssinc',
+    description: 'ViteでSSIライクな `#include` をするためのプラグイン',
+    url: 'https://www.npmjs.com/package/vite-plugin-ssinc',
   },
 ];
