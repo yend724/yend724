07ebb21de7124bd69c480c3c3ec738b44ddbe5c4
yend724 <65233817+yend724@users.noreply.github.com>
2024-12-17 10:59:02 +0900

    wip

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,4 +4,5 @@ export const SOCIAL_LINKS = [
   { label: 'Qiita', href: 'https://qiita.com/yend724' },
   { label: 'GitHub', href: 'https://github.com/yend724' },
   { label: 'CodePen', href: 'https://codepen.io/yend24' },
+  { label: 'mixi2', href: 'https://mixi.social/@yend724' },
 ];
