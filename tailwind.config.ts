8d5677d7ae8a9b9958b5a71b504a866c10ac0c36
yend724 <yend9524@gmail.com>
2024-12-20 23:08:09 +0900

    wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     lineClampPlugin,
     plugin(({ addVariant }) => {
       addVariant('hover', '@media(any-hover:hover){ &:hover }');
+      addVariant('group-hover', '@media(any-hover:hover){ .group:hover & }');
     }),
   ],
 };
